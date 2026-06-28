e0b235c2a88fdc399423018d4e7cabf16f23afef
SqaureFeetProfessionals <gaurav.wadhwa234@gmail.com>
2026-06-29 00:31:36 +0530

    new new

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const path = require('path');
 // Load environment variables
 dotenv.config();
 
+// test
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  'http://localhost:5173', 
-  'http://localhost:3000', 
+  'http://localhost:5173',
+  'http://localhost:3000',
   'http://localhost:5000',
   'https://vasturentals.vercel.app'
 ];
